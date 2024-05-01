e35ae3ce7c4ee9a768e3951bac5ed5d7ac41820d
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-30 23:48:53 -0600

    code.js

--- code.js
+++ code.js
@@ -1,3 +1,22 @@
 function augmentingPath(graph, start, end) {
-    return [];
+    function dfs(node, visited, path) {
+        visited[node] = true;
+        if (node === end) {
+            path.push(node);
+            return path;
+        }
+        for (let neighbor of graph[node]) {
+            if (!visited[neighbor.name] && neighbor.capacity > 0) {
+                let result = dfs(neighbor.name, visited, path.concat([node]));
+                if (result.length > 0) {
+                    return result;
+                }
+            }
+        }
+        return [];
+    }
+
+    let visited = {};
+    let path = dfs(start, visited, []);
+    return path;
 }
